4f86ce91d01264e687480541d6068169b185ccb1
Christopher Zhu <christopherzhu@Chriss-MacBook-Pro.local>
2020-03-08 14:09:48 -0500

    Revert "work on buttons"
    This reverts commit d8c8417da119e70db3525757ae6afc2c6f540a31.

--- shevotesillinois/src/components/legrecap.vue
+++ shevotesillinois/src/components/legrecap.vue
@@ -34,21 +34,6 @@
       <b-button variant="primary" href="#">Reproductive Health Act</b-button>
       <b-button variant="primary" href="#">Reproductive Health Act</b-button>
       <b-button variant="primary" href="#">Reproductive Health Act</b-button>
-      <div v-for="(section, index) in Object.keys(entries)" :key="index" class="group">
-        <h2 class="center">{{section}}</h2>
-        <div class="section" v-for="entry in entries[section]" :key="entry.id">
-          <button id="billBtn" variant="primary" >{{entry.title}}</button>
-          <!-- The Modal -->
-          <div id="billBtn" class="modal">
-
-            <!-- Modal content -->
-            <div class="modal-content">
-              <span class="close">&times;</span>
-              <p>Some text in the Modal..</p>
-             </div>
-          </div>
-        </div>
-      </div>
     </b-jumbotron>
 
     <b-jumbotron>
@@ -134,16 +119,3 @@
   </b-container>
   </div>
 </template>
-
-<script>
-import LEGENTRIES from '@/assets/legislation_recap.json';
-
-export default {
-  name: 'home',
-  computed: {
-    entries() {
-      return LEGENTRIES;
-    },
-  },
-};
-</script>
